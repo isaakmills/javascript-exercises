2ffefd2241e08dbfbb39ec4c36db3b2ee197af72
Isaak Mills <isaakmills@gmail.com>
2024-06-14 11:46:52 -0700

    completed palindromes

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,23 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    
+    strArray = string.split();
+    return strArray.reverse
+    
+    
+ /*   
+    strLength  = string.length;
+    const letterArray = []
+    for(let i = 0; i < strLength; i++){
+        letterArray.push();
+    }
+    return letterArray.toString();
+     //return letterArray.reverse().join();
+
+*/
+//for each character in a string, store that
+//character inside an array. reverse array and
+//return array  
+
 
 };
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,35 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    strArray = str.toLowerCase().split("").filter(char => char  !== " " & char !=="," & char !=="." & char !=="!" );
+        console.log(strArray.join(""))
 
+
+    if(strArray.join("") == strArray.reverse().join("")){ 
+        //console.log(strArray)
+        return true
+    } else{
+        return false};
+
+
+
+
+    console.log(typeof strArray)
+    console.log(strArray.reverse().join())
+    console.log(strArray.join())
 };
 
 // Do not edit below this line
+
+
+
+
+
 module.exports = palindromes;
+
+
+//.join()
+/*  
+remove spaces
+remove  , and . 
+remove capitals
+
+*/
